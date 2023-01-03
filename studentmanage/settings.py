e8522f7a19e3bb513d2455e3d9d47207f953308d
Harinderkumar1 <111347499+Harinderkumar1@users.noreply.github.com>
2023-01-03 15:31:22 +0530

    Update settings.py

--- studentmanage/settings.py
+++ studentmanage/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-%5h7ky0!qu!f2s-7r93o&ba5+c*nsvagdheg)^ucu3#7sphz-1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
